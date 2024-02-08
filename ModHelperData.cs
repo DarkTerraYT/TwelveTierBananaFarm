c633b76c7d30c53eee7904bd05bc6d7460cc868d
CubedLeaves <105320711+DarkTerraYT@users.noreply.github.com>
2024-02-07 22:04:42 -0500

    Update ModHelperData.cs

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -6,7 +6,7 @@ public static class ModHelperData
     public const string Version = "1.0.0";
     public const string Name = "Twelve Tier Banana Farm";
 
-    public const string Description = "Adds 7 new Banana Farm Tiers on the top Path. I plan on doing the two other path eventually.\n\nThis mod is crazy.\n\nCrazy? I was crazy once...";
+    public const string Description = "Adds 7 new Banana Farm Tiers on the top Path. I plan on doing the two other path eventually.\n\nThis mod is crazy.\n\nCrazy? I was crazy once... for mod ideas contact me on discord: @lerpmcgerk";
 
     public const string RepoOwner = "DarkTerraYT"; // TODO add your github username hero, also in the download url in README.md
     public const string RepoName = "TwelveTierBananaFarm"; // TODO add your repo name here, also in the download url in README.md
